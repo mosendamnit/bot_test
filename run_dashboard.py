94a1403a749fe65a9e533538a3ba46ce0702da94
mosendamnit <mohsin_afridi99@hotmail.com>
2024-10-23 08:05:02 +0200

    minor fxing

--- run_dashboard.py
+++ run_dashboard.py
@@ -2,5 +2,5 @@ import subprocess
 from pathlib import Path
 
 if __name__ == "__main__":
-    dashboard_path = Path(__file__).parent / "chat.py"
+    dashboard_path = Path(__file__).parent / "dashboard.py"
     subprocess.run(f"streamlit run '{dashboard_path}'", shell=True)
